1262a8676d9e310612b0a6624c5f0901dd724d56
adrianalvarezf <adrian.alvarez.fernandez@cern.ch>
2019-01-31 16:55:37 +0100

    removed old things

--- code_muon_fakes/nuisances_none.py
+++
@@ -1,4 +0,0 @@
-
-# nuisances (none , test)
-
-nuisances = {}
--- launch_MC_Z.sh
+++
@@ -1,7 +0,0 @@
-bsub -q 2nd -u alvareza submit_batch_FR_Z.sh nanoLatino_WZ__part2.root
-bsub -q 2nd -u alvareza submit_batch_FR_Z.sh nanoLatino_ZZ__part1.root
-bsub -q 2nd -u alvareza submit_batch_FR_Z.sh nanoLatino_WZ__part0.root
-bsub -q 2nd -u alvareza submit_batch_FR_Z.sh nanoLatino_ZZ__part2.root
-bsub -q 2nd -u alvareza submit_batch_FR_Z.sh nanoLatino_ZZ__part0.root
-bsub -q 2nd -u alvareza submit_batch_FR_Z.sh nanoLatino_WZ__part3.root
-bsub -q 2nd -u alvareza submit_batch_FR_Z.sh nanoLatino_WZ__part1.root
